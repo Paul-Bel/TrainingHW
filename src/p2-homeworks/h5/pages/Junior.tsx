bf3262616a249d72b1cba1c89676ae8a0ae6bc82
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-01-24 20:37:40 +0300

    HW 8 si finish

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -11,7 +11,7 @@ export const Junior = () => {
         <div>
             <HW7/>
             <HW8/>
-            {/*<HW9/>*/}
+            <HW9/>
             {/*<HW10/>*/}
             {/*<HW11/>*/}
         </div>
