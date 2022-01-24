03b364a1668510b19c26762455bd28f76ba0ed66
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-01-25 00:53:45 +0300

    added  JQuery

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -19,22 +19,20 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
         onChangeRange([+e.currentTarget.value, value[0]])
     }
 
-    $( function() {
-
-        $( "#slider-range" ).slider({
-            range: true,
-            min: 0,
-            max: 500,
-            values: [ 75, 300 ],
-
-            slide: function( event, ui ) {
-                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-            }
-        });
-        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-            //@ts-ignore
-            " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-    } );
+    // $( function() {
+    //     $( "#slider-range" ).slider({
+    //         range: true,
+    //         min: 0,
+    //         max: 500,
+    //         values: [ 75, 300 ],
+    //         slide: function( event, ui ) {
+    //             $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    //         }
+    //     });
+    //     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    //         //@ts-ignore
+    //         " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    // } );
 
     return (
         <>
@@ -43,7 +41,7 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
                 value={value[0]}
                 onChange={onChangeCallback}
             />
-            <div id="slider-range"></div>
+            {/*<div id="slider-range"></div>*/}
         </>
     )
 }
